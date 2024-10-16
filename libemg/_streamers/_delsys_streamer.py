f923be9134cc569982e4b61ce74cea0975ab88a7
Mateusz Szumilas <m.szumilas@mchtr.pw.edu.pl>
2024-10-16 17:41:34 +0200

    tab corrections

--- libemg/_streamers/_delsys_streamer.py
+++ libemg/_streamers/_delsys_streamer.py
@@ -80,10 +80,10 @@ class DelsysEMGStreamer(Process):
         self.channel_list = channel_list
         self.timeout = timeout
 		
-		self.smm_initialized = Event()
+        self.smm_initialized = Event()
 
         self._min_recv_size = 16 * self.BYTES_PER_CHANNEL
-		self._min_aux_recv_size = (9 * 16) * self.BYTES_PER_CHANNEL
+        self._min_aux_recv_size = (9 * 16) * self.BYTES_PER_CHANNEL
 
     def connect(self):
         # create command socket and consume the servers initial response
@@ -113,7 +113,7 @@ class DelsysEMGStreamer(Process):
         self.smm = SharedMemoryManager()
         for item in self.shared_memory_items:
             self.smm.create_variable(*item)
-		self.smm_initialized.set()
+        self.smm_initialized.set()
 
         def write_emg(emg):
             # update the samples in "emg"
@@ -148,11 +148,11 @@ class DelsysEMGStreamer(Process):
                     assert np.any(data!=0), "IMU not currently working"
                     if len(data.shape)==1:
                         data = data.reshape((-1,9))[self.channel_list, :]
-						data = data.reshape(-1)[None, :]
-					else: 
-						data = np.reshape(data, (data.shape[0], 9, -1))
-						data = data[:,self.channel_list,:]
-						data = np.reshape(data, (data.shape[0], -1))
+                        data = data.reshape(-1)[None, :]
+                    else: 
+                        data = np.reshape(data, (data.shape[0], 9, -1))
+                        data = data[:,self.channel_list,:]
+                        data = np.reshape(data, (data.shape[0], -1))
                     for i in self.imu_handlers:
                         i(data)
                 A = 1
